9894fcea574649e618a046114c5b8879530fe37e
Joe Taylor <joe@textninja.net>
2022-05-05 09:33:51 -0700

    Refactor for concision

--- get-billing-info/index.js
+++ get-billing-info/index.js
@@ -2,6 +2,7 @@ const billing = require('@google-cloud/billing');
 const fs = require('fs').promises;
 
 /*
+
 Unique category.resourceGroup values:
 [
   "RAM",
@@ -11,15 +12,19 @@ Unique category.resourceGroup values:
   "F1Micro",
   "G1Small"
 ]
+
 */
 
 exports.getSkusData = async (req, res) => {
-  const skusData = JSON.parse(await fs.readFile('skus.json', 'utf-8'));
-  const usWestSkus = skusData.filter(sku => sku.serviceRegions.includes("us-west1"));
-  const computeSkus = usWestSkus.filter(sku => sku.category.resourceFamily === 'Compute');
-  const usageSkus = computeSkus.filter(sku => ["OnDemand", "Preemptible"].includes(sku.category.usageType));
-  const relevantSkus = usageSkus.filter(sku => ["RAM", "CPU", "N1Standard", "F1Micro", "G1Small"].includes(sku.category.resourceGroup));
-  
+  const relevantSkus = JSON.parse(await fs.readFile('skus.json', 'utf-8'))
+    .filter(sku => sku.serviceRegions.includes("us-west1"))
+    .filter(sku => sku.category.resourceFamily === 'Compute')
+    .filter(sku => ["OnDemand", "Preemptible"].includes(sku.category.usageType))
+    .filter(
+      sku => ["RAM", "CPU", "N1Standard", "F1Micro", "G1Small"]
+                .includes(sku.category.resourceGroup)
+    )
+
   res.status(200).send(relevantSkus);
 };
 
